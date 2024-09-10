1fe7495fabebcaf79b63705d81f0ff89a3f8bc0d
Lakshya429 <lakshyathakur429@gmail.com>
2024-09-10 17:53:51 +0530

    t4

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,17 +1,32 @@
 
 import GoogleProvider from "next-auth/providers/google";
-
+import NextAuth, {type DefaultSession } from "next-auth"
 import { prismaClient } from "@/app/lib/db";
+import Github from "next-auth/providers/github";
 
 
-export const authOption =  {
+declare module "next-auth" {
+    interface Session {
+        user: {
+            id: string
+        } & DefaultSession["user"]
+    }
+}
+export const authOption = {
     providers: [
+
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        }),
+
+        Github({
+            clientId : process.env.GITHUB_CLIENT_ID ?? " ",
+            clientSecret : process.env.GITHUB_CLIENT_SECRET ?? ""
         })
     ],
     secret: process.env.NEXTAUTH_SECRET ?? "secret",
+
     callbacks: {
         async signIn(params : any) {
             console.log(params);
@@ -19,35 +34,29 @@ export const authOption =  {
             if (!params.user.email) {
                 return false;
             }
-
             try {
-                console.log(params);
-                
                 const existingUser = await prismaClient.user.findUnique({
                     where: {
                         email: params.user.email
                     }
                 })
-                
-                console.log(existingUser);
-                
                 if (existingUser) {
                     return true
                 }
-
                 await prismaClient.user.create({
                     data: {
                         email: params.user.email,
-                        provider: "Google"
+                        provider: params.account?.provider || " "
                     } 
                 })
                 return true;
              } catch(e) {
                 console.log(e);
                 return false;
-             }
+             } 
         },
-        async session({session} : any) {
+        
+        async session({session, token, user} : any) {
             const dbUser = await prismaClient.user.findUnique({
                 where: {
                     email: session.user.email as string
